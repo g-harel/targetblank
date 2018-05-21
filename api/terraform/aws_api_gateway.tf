a6c7380ac71710abf610c6dace72f6e30d81a784
Gabriel <gabrielj.harel@gmail.com>
2018-05-21 12:08:26 -0400

    removed api/v1 from api gateway path

--- api/terraform/aws_api_gateway.tf
+++ api/terraform/aws_api_gateway.tf
@@ -4,15 +4,15 @@ resource "aws_api_gateway_rest_api" "gateway" {
 
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [
-    "aws_api_gateway_method.api_v1_auth_addr_delete",
-    "aws_api_gateway_method.api_v1_auth_addr_post",
-    "aws_api_gateway_method.api_v1_auth_addr_put",
-    "aws_api_gateway_method.api_v1_page_addr_delete",
-    "aws_api_gateway_method.api_v1_page_addr_get",
-    "aws_api_gateway_method.api_v1_page_addr_patch",
-    "aws_api_gateway_method.api_v1_page_addr_put",
-    "aws_api_gateway_method.api_v1_page_post",
-    "aws_api_gateway_method.api_v1_page_validate_post",
+    "aws_api_gateway_method.auth_addr_delete",
+    "aws_api_gateway_method.auth_addr_post",
+    "aws_api_gateway_method.auth_addr_put",
+    "aws_api_gateway_method.page_addr_delete",
+    "aws_api_gateway_method.page_addr_get",
+    "aws_api_gateway_method.page_addr_patch",
+    "aws_api_gateway_method.page_addr_put",
+    "aws_api_gateway_method.page_post",
+    "aws_api_gateway_method.page_validate_post",
   ]
 
   rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
@@ -24,62 +24,52 @@ resource "aws_api_gateway_deployment" "deployment" {
 #
 #
 
-resource "aws_api_gateway_resource" "api" {
+resource "aws_api_gateway_resource" "auth" {
   rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
   parent_id   = "${aws_api_gateway_rest_api.gateway.root_resource_id}"
-  path_part   = "api"
-}
-
-resource "aws_api_gateway_resource" "api_v1" {
-  rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
-  parent_id   = "${aws_api_gateway_resource.api.id}"
-  path_part   = "v1"
-}
-
-resource "aws_api_gateway_resource" "api_v1_auth" {
-  rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
-  parent_id   = "${aws_api_gateway_resource.api_v1.id}"
   path_part   = "auth"
 }
 
-resource "aws_api_gateway_resource" "api_v1_page" {
+resource "aws_api_gateway_resource" "page" {
   rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
-  parent_id   = "${aws_api_gateway_resource.api_v1.id}"
+  parent_id   = "${aws_api_gateway_rest_api.gateway.root_resource_id}"
   path_part   = "page"
 }
 
-resource "aws_api_gateway_resource" "api_v1_auth_addr" {
+#
+
+resource "aws_api_gateway_resource" "auth_addr" {
   rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
-  parent_id   = "${aws_api_gateway_resource.api_v1_auth.id}"
+  parent_id   = "${aws_api_gateway_resource.auth.id}"
   path_part   = "{addr}"
 }
 
-resource "aws_api_gateway_resource" "api_v1_page_addr" {
+resource "aws_api_gateway_resource" "page_addr" {
   rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
-  parent_id   = "${aws_api_gateway_resource.api_v1_page.id}"
+  parent_id   = "${aws_api_gateway_resource.page.id}"
   path_part   = "{addr}"
 }
 
-resource "aws_api_gateway_resource" "api_v1_page_validate" {
+resource "aws_api_gateway_resource" "page_validate" {
   rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
-  parent_id   = "${aws_api_gateway_resource.api_v1_page.id}"
+  parent_id   = "${aws_api_gateway_resource.page.id}"
   path_part   = "validate"
 }
 
 #
 #
 
-resource "aws_api_gateway_method" "api_v1_auth_addr_delete" {
+resource "aws_api_gateway_method" "auth_addr_delete" {
   rest_api_id   = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id   = "${aws_api_gateway_resource.api_v1_auth_addr.id}"
+  resource_id   = "${aws_api_gateway_resource.auth_addr.id}"
   http_method   = "DELETE"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "api_v1_auth_addr_delete" {
+resource "aws_api_gateway_integration" "auth_addr_delete" {
   rest_api_id             = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id             = "${aws_api_gateway_resource.api_v1_auth_addr.id}"
-  http_method             = "${aws_api_gateway_method.api_v1_auth_addr_delete.http_method}"
+  resource_id             = "${aws_api_gateway_resource.auth_addr.id}"
+  http_method             = "${aws_api_gateway_method.auth_addr_delete.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.password_reset.arn}/invocations"
@@ -87,17 +77,17 @@ resource "aws_api_gateway_integration" "api_v1_auth_addr_delete" {
 
 #
 
-resource "aws_api_gateway_method" "api_v1_auth_addr_post" {
+resource "aws_api_gateway_method" "auth_addr_post" {
   rest_api_id   = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id   = "${aws_api_gateway_resource.api_v1_auth_addr.id}"
+  resource_id   = "${aws_api_gateway_resource.auth_addr.id}"
   http_method   = "POST"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "api_v1_auth_addr_post" {
+resource "aws_api_gateway_integration" "auth_addr_post" {
   rest_api_id             = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id             = "${aws_api_gateway_resource.api_v1_auth_addr.id}"
-  http_method             = "${aws_api_gateway_method.api_v1_auth_addr_post.http_method}"
+  resource_id             = "${aws_api_gateway_resource.auth_addr.id}"
+  http_method             = "${aws_api_gateway_method.auth_addr_post.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.authenticate.arn}/invocations"
@@ -105,17 +95,17 @@ resource "aws_api_gateway_integration" "api_v1_auth_addr_post" {
 
 #
 
-resource "aws_api_gateway_method" "api_v1_auth_addr_put" {
+resource "aws_api_gateway_method" "auth_addr_put" {
   rest_api_id   = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id   = "${aws_api_gateway_resource.api_v1_auth_addr.id}"
+  resource_id   = "${aws_api_gateway_resource.auth_addr.id}"
   http_method   = "PUT"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "api_v1_auth_addr_put" {
+resource "aws_api_gateway_integration" "auth_addr_put" {
   rest_api_id             = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id             = "${aws_api_gateway_resource.api_v1_auth_addr.id}"
-  http_method             = "${aws_api_gateway_method.api_v1_auth_addr_put.http_method}"
+  resource_id             = "${aws_api_gateway_resource.auth_addr.id}"
+  http_method             = "${aws_api_gateway_method.auth_addr_put.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.password_change.arn}/invocations"
@@ -123,17 +113,17 @@ resource "aws_api_gateway_integration" "api_v1_auth_addr_put" {
 
 #
 
-resource "aws_api_gateway_method" "api_v1_page_validate_post" {
+resource "aws_api_gateway_method" "page_validate_post" {
   rest_api_id   = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id   = "${aws_api_gateway_resource.api_v1_page_validate.id}"
+  resource_id   = "${aws_api_gateway_resource.page_validate.id}"
   http_method   = "POST"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "api_v1_page_validate_post" {
+resource "aws_api_gateway_integration" "page_validate_post" {
   rest_api_id             = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id             = "${aws_api_gateway_resource.api_v1_page_validate.id}"
-  http_method             = "${aws_api_gateway_method.api_v1_page_validate_post.http_method}"
+  resource_id             = "${aws_api_gateway_resource.page_validate.id}"
+  http_method             = "${aws_api_gateway_method.page_validate_post.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.page_validate.arn}/invocations"
@@ -141,17 +131,17 @@ resource "aws_api_gateway_integration" "api_v1_page_validate_post" {
 
 #
 
-resource "aws_api_gateway_method" "api_v1_page_post" {
+resource "aws_api_gateway_method" "page_post" {
   rest_api_id   = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id   = "${aws_api_gateway_resource.api_v1_page.id}"
+  resource_id   = "${aws_api_gateway_resource.page.id}"
   http_method   = "POST"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "api_v1_page_post" {
+resource "aws_api_gateway_integration" "page_post" {
   rest_api_id             = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id             = "${aws_api_gateway_resource.api_v1_page.id}"
-  http_method             = "${aws_api_gateway_method.api_v1_page_post.http_method}"
+  resource_id             = "${aws_api_gateway_resource.page.id}"
+  http_method             = "${aws_api_gateway_method.page_post.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.create.arn}/invocations"
@@ -159,17 +149,17 @@ resource "aws_api_gateway_integration" "api_v1_page_post" {
 
 #
 
-resource "aws_api_gateway_method" "api_v1_page_addr_delete" {
+resource "aws_api_gateway_method" "page_addr_delete" {
   rest_api_id   = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id   = "${aws_api_gateway_resource.api_v1_page_addr.id}"
+  resource_id   = "${aws_api_gateway_resource.page_addr.id}"
   http_method   = "DELETE"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "api_v1_page_addr_delete" {
+resource "aws_api_gateway_integration" "page_addr_delete" {
   rest_api_id             = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id             = "${aws_api_gateway_resource.api_v1_page_addr.id}"
-  http_method             = "${aws_api_gateway_method.api_v1_page_addr_delete.http_method}"
+  resource_id             = "${aws_api_gateway_resource.page_addr.id}"
+  http_method             = "${aws_api_gateway_method.page_addr_delete.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.delete.arn}/invocations"
@@ -177,17 +167,17 @@ resource "aws_api_gateway_integration" "api_v1_page_addr_delete" {
 
 #
 
-resource "aws_api_gateway_method" "api_v1_page_addr_get" {
+resource "aws_api_gateway_method" "page_addr_get" {
   rest_api_id   = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id   = "${aws_api_gateway_resource.api_v1_page_addr.id}"
+  resource_id   = "${aws_api_gateway_resource.page_addr.id}"
   http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "api_v1_page_addr_get" {
+resource "aws_api_gateway_integration" "page_addr_get" {
   rest_api_id             = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id             = "${aws_api_gateway_resource.api_v1_page_addr.id}"
-  http_method             = "${aws_api_gateway_method.api_v1_page_addr_get.http_method}"
+  resource_id             = "${aws_api_gateway_resource.page_addr.id}"
+  http_method             = "${aws_api_gateway_method.page_addr_get.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.page_fetch.arn}/invocations"
@@ -195,17 +185,17 @@ resource "aws_api_gateway_integration" "api_v1_page_addr_get" {
 
 #
 
-resource "aws_api_gateway_method" "api_v1_page_addr_patch" {
+resource "aws_api_gateway_method" "page_addr_patch" {
   rest_api_id   = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id   = "${aws_api_gateway_resource.api_v1_page_addr.id}"
+  resource_id   = "${aws_api_gateway_resource.page_addr.id}"
   http_method   = "PATCH"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "api_v1_page_addr_patch" {
+resource "aws_api_gateway_integration" "page_addr_patch" {
   rest_api_id             = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id             = "${aws_api_gateway_resource.api_v1_page_addr.id}"
-  http_method             = "${aws_api_gateway_method.api_v1_page_addr_patch.http_method}"
+  resource_id             = "${aws_api_gateway_resource.page_addr.id}"
+  http_method             = "${aws_api_gateway_method.page_addr_patch.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.publish.arn}/invocations"
@@ -213,17 +203,17 @@ resource "aws_api_gateway_integration" "api_v1_page_addr_patch" {
 
 #
 
-resource "aws_api_gateway_method" "api_v1_page_addr_put" {
+resource "aws_api_gateway_method" "page_addr_put" {
   rest_api_id   = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id   = "${aws_api_gateway_resource.api_v1_page_addr.id}"
+  resource_id   = "${aws_api_gateway_resource.page_addr.id}"
   http_method   = "PUT"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "api_v1_page_addr_put" {
+resource "aws_api_gateway_integration" "page_addr_put" {
   rest_api_id             = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id             = "${aws_api_gateway_resource.api_v1_page_addr.id}"
-  http_method             = "${aws_api_gateway_method.api_v1_page_addr_put.http_method}"
+  resource_id             = "${aws_api_gateway_resource.page_addr.id}"
+  http_method             = "${aws_api_gateway_method.page_addr_put.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.page_edit.arn}/invocations"
