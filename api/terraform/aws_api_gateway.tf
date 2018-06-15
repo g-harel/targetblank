ebe5882fe96e93cbf3f9f5037466887c901cfc6e
Gabriel <gabrielj.harel@gmail.com>
2018-06-14 23:45:36 -0400

    got cors working on chrome. firefox still complaining

--- api/terraform/aws_api_gateway.tf
+++ api/terraform/aws_api_gateway.tf
@@ -67,6 +67,38 @@ resource "aws_api_gateway_method_response" "auth_options" {
   depends_on = ["aws_api_gateway_method.auth_options"]
 }
 
+resource "aws_api_gateway_integration" "auth_options" {
+  rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
+  resource_id = "${aws_api_gateway_resource.auth.id}"
+  http_method = "${aws_api_gateway_method.auth_options.http_method}"
+  type        = "MOCK"
+
+  passthrough_behavior = "WHEN_NO_MATCH"
+
+  request_templates {
+    "application/json" = "{ 'statusCode': 200 }"
+  }
+
+  depends_on = ["aws_api_gateway_method.auth_options"]
+}
+
+resource "aws_api_gateway_integration_response" "auth_options" {
+  rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
+  resource_id = "${aws_api_gateway_resource.auth.id}"
+  http_method = "${aws_api_gateway_method.auth_options.http_method}"
+  status_code = "${aws_api_gateway_method_response.auth_options.status_code}"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'*'"
+    "method.response.header.Access-Control-Allow-Methods" = "'*'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = ["aws_api_gateway_method_response.auth_options"]
+}
+
+#
+
 resource "aws_api_gateway_resource" "page" {
   rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
   parent_id   = "${aws_api_gateway_rest_api.rest_api.root_resource_id}"
@@ -97,6 +129,36 @@ resource "aws_api_gateway_method_response" "page_options" {
   }
 
   depends_on = ["aws_api_gateway_method.page_options"]
+}
+
+resource "aws_api_gateway_integration" "page_options" {
+  rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
+  resource_id = "${aws_api_gateway_resource.page.id}"
+  http_method = "${aws_api_gateway_method.page_options.http_method}"
+  type        = "MOCK"
+
+  passthrough_behavior = "WHEN_NO_MATCH"
+
+  request_templates {
+    "application/json" = "{ 'statusCode': 200 }"
+  }
+
+  depends_on = ["aws_api_gateway_method.page_options"]
+}
+
+resource "aws_api_gateway_integration_response" "page_options" {
+  rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
+  resource_id = "${aws_api_gateway_resource.page.id}"
+  http_method = "${aws_api_gateway_method.page_options.http_method}"
+  status_code = "${aws_api_gateway_method_response.page_options.status_code}"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'*'"
+    "method.response.header.Access-Control-Allow-Methods" = "'*'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = ["aws_api_gateway_method_response.page_options"]
 }
 
 #
@@ -133,6 +195,38 @@ resource "aws_api_gateway_method_response" "auth_addr_options" {
   depends_on = ["aws_api_gateway_method.auth_addr_options"]
 }
 
+resource "aws_api_gateway_integration" "auth_addr_options" {
+  rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
+  resource_id = "${aws_api_gateway_resource.auth_addr.id}"
+  http_method = "${aws_api_gateway_method.auth_addr_options.http_method}"
+  type        = "MOCK"
+
+  passthrough_behavior = "WHEN_NO_MATCH"
+
+  request_templates {
+    "application/json" = "{ 'statusCode': 200 }"
+  }
+
+  depends_on = ["aws_api_gateway_method.auth_addr_options"]
+}
+
+resource "aws_api_gateway_integration_response" "auth_addr_options" {
+  rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
+  resource_id = "${aws_api_gateway_resource.auth_addr.id}"
+  http_method = "${aws_api_gateway_method.auth_addr_options.http_method}"
+  status_code = "${aws_api_gateway_method_response.auth_addr_options.status_code}"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'*'"
+    "method.response.header.Access-Control-Allow-Methods" = "'*'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = ["aws_api_gateway_method_response.auth_addr_options"]
+}
+
+#
+
 resource "aws_api_gateway_resource" "page_addr" {
   rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
   parent_id   = "${aws_api_gateway_resource.page.id}"
@@ -165,6 +259,38 @@ resource "aws_api_gateway_method_response" "page_addr_options" {
   depends_on = ["aws_api_gateway_method.page_addr_options"]
 }
 
+resource "aws_api_gateway_integration" "page_addr_options" {
+  rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
+  resource_id = "${aws_api_gateway_resource.page_addr.id}"
+  http_method = "${aws_api_gateway_method.page_addr_options.http_method}"
+  type        = "MOCK"
+
+  passthrough_behavior = "WHEN_NO_MATCH"
+
+  request_templates {
+    "application/json" = "{ 'statusCode': 200 }"
+  }
+
+  depends_on = ["aws_api_gateway_method.page_addr_options"]
+}
+
+resource "aws_api_gateway_integration_response" "page_addr_options" {
+  rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
+  resource_id = "${aws_api_gateway_resource.page_addr.id}"
+  http_method = "${aws_api_gateway_method.page_addr_options.http_method}"
+  status_code = "${aws_api_gateway_method_response.page_addr_options.status_code}"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'*'"
+    "method.response.header.Access-Control-Allow-Methods" = "'*'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = ["aws_api_gateway_method_response.page_addr_options"]
+}
+
+#
+
 resource "aws_api_gateway_resource" "page_validate" {
   rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
   parent_id   = "${aws_api_gateway_resource.page.id}"
@@ -195,6 +321,36 @@ resource "aws_api_gateway_method_response" "page_validate_options" {
   }
 
   depends_on = ["aws_api_gateway_method.page_validate_options"]
+}
+
+resource "aws_api_gateway_integration" "page_validate_options" {
+  rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
+  resource_id = "${aws_api_gateway_resource.page_validate.id}"
+  http_method = "${aws_api_gateway_method.page_validate_options.http_method}"
+  type        = "MOCK"
+
+  passthrough_behavior = "WHEN_NO_MATCH"
+
+  request_templates {
+    "application/json" = "{ 'statusCode': 200 }"
+  }
+
+  depends_on = ["aws_api_gateway_method.page_validate_options"]
+}
+
+resource "aws_api_gateway_integration_response" "page_validate_options" {
+  rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
+  resource_id = "${aws_api_gateway_resource.page_validate.id}"
+  http_method = "${aws_api_gateway_method.page_validate_options.http_method}"
+  status_code = "${aws_api_gateway_method_response.page_validate_options.status_code}"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'*'"
+    "method.response.header.Access-Control-Allow-Methods" = "'*'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = ["aws_api_gateway_method_response.page_validate_options"]
 }
 
 #
