013aa5941984d6c8da191b957f1c7a88dc296351
Gabriel <gabrielj.harel@gmail.com>
2018-08-12 22:30:19 -0400

    style error display

--- app/components/errors.ts
+++ app/components/errors.ts
@@ -1,14 +1,24 @@
+import "../static/component.errors.scss";
+
 export interface IErrorProps {
     errors: string[];
     hide: () => void;
 }
 
-export const errors = ({errors}: IErrorProps) => () => (
-    ["div.errors", {} ,
-        errors.map((err) => (
+export const errors = ({errors, hide}: IErrorProps) => () => (
+    ["div.errors", {
+        className: {hidden: !errors.length},
+    }, [
+        ["div.title", {}, [
+            "error",
+            ["div.dismiss", {onclick: hide}, [
+                "dismiss",
+            ]],
+        ]],
+        ...errors.map((err) => (
             ["div.error", {}, [
                 err,
             ]]
         ))
-    ]
+    ]]
 );
