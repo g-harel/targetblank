b4cfcee89d1e18c8b0ddc4e5b3bcb4ac303c9e2b
Gabriel <gabrielj.harel@gmail.com>
2019-01-08 23:10:38 -0500

    reintroduce removed tests

--- internal/parser/document_test.go
+++ internal/parser/document_test.go
@@ -3,7 +3,9 @@ package parser
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"regexp"
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -84,7 +86,178 @@ func produceErr(t *testing.T, line int, pattern string, s ...string) {
 	}
 }
 
-func TestParser(t *testing.T) {
+func TestDocumentAddGroup(t *testing.T) {
+	t.Run("should add a new group to the document", func(t *testing.T) {
+		doc := newDocument()
+		qty := len(doc.Groups)
+		doc.AddGroup()
+
+		if len(doc.Groups) <= qty {
+			t.Errorf("Group was not added to document")
+		}
+	})
+}
+
+func TestDocumentAddGroupMeta(t *testing.T) {
+	t.Run("should add values to last group's meta", func(t *testing.T) {
+		doc := newDocument()
+		key := "key"
+		value := "value"
+		doc.AddGroupMeta(key, value)
+
+		if doc.Groups[0].Meta[key] != value {
+			t.Errorf("Incorrect meta value (expected : \"%v\", received: \"%v\")", value, doc.Meta[key])
+		}
+	})
+}
+
+func TestDocumentEnter(t *testing.T) {
+	t.Run("should add the entry to the last group when depth is zero", func(t *testing.T) {
+		for i := 0; i < 3; i++ {
+			doc := newDocument()
+			label := fmt.Sprintf("%d", i)
+
+			for j := 0; j < i; j++ {
+				doc.AddGroup()
+			}
+
+			err := doc.Enter(0, "link", label)
+			if err != nil {
+				t.Fatalf("Unexpected error: %s", err)
+			}
+
+			if doc.Groups[len(doc.Groups)-1].Entries[0].Label != label {
+				t.Errorf("Item was not added to the correct group")
+			}
+		}
+	})
+
+	t.Run("should not allow negative depths", func(t *testing.T) {
+		doc := newDocument()
+		err := doc.Enter(-1, "", "")
+		if err == nil {
+			t.Fatalf("Expected invalid depth error")
+		}
+		if strings.Index(err.Error(), "invalid depth") == -1 {
+			t.Fatalf("Expected invalid depth error, but got: %s", err)
+		}
+	})
+
+	t.Run("should not allow depths to be skipped", func(t *testing.T) {
+		doc := newDocument()
+		err := doc.Enter(0, "", "")
+		if err != nil {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+
+		err = doc.Enter(2, "", "")
+		if err == nil {
+			t.Fatalf("Expected invalid depth error")
+		}
+		if strings.Index(err.Error(), "invalid depth") == -1 {
+			t.Fatalf("Expected invalid depth error, but got: %s", err)
+		}
+	})
+
+	t.Run("should trim whitespace from both link and label", func(t *testing.T) {
+		link := " link "
+		label := " label "
+		doc := newDocument()
+		doc.Enter(0, link, label)
+
+		if doc.Groups[0].Entries[0].Link != strings.TrimSpace(link) {
+			t.Errorf("Whitespace was not trimmed from :%s", link)
+		}
+		if doc.Groups[0].Entries[0].Label != strings.TrimSpace(label) {
+			t.Errorf("Whitespace was not trimmed from :%s", label)
+		}
+	})
+
+	t.Run("should use label as link if it resembles a url", func(t *testing.T) {
+		expectURL := func(isURL bool, label string) {
+			doc := newDocument()
+			doc.Enter(0, "", label)
+			if isURL {
+				if doc.Groups[0].Entries[0].Link != label {
+					t.Errorf("Label should have been used as link: \"%v\"", label)
+				}
+			} else {
+				if doc.Groups[0].Entries[0].Link == label {
+					t.Errorf("Label should not have been used as link: \"%v\"", label)
+				}
+			}
+		}
+
+		expectURL(true, "https://example.com/test")
+		expectURL(true, "www.example.com")
+		expectURL(true, "example.com?q=test")
+		expectURL(true, "localhost:8080")
+		expectURL(false, "example: Example")
+		expectURL(false, "ExampleExample")
+		expectURL(false, "Example example example")
+	})
+
+	t.Run("should correctly append entries", func(t *testing.T) {
+		doc := newDocument()
+
+		label := func(a []int) string {
+			l := make([]string, len(a))
+			for i, n := range a {
+				l[i] = strconv.Itoa(n)
+			}
+			return strings.Join(l, ".")
+		}
+
+		check := func(depth int, address ...int) {
+			if len(address) < 1 {
+				t.Fatalf("Empty address passed as argument")
+			}
+
+			l := label(address)
+			err := doc.Enter(depth, l, "")
+			if err != nil {
+				t.Fatalf("Unexpected error when adding Item{depth:%v,label:\"%v\"}: %v", depth, l, err)
+			}
+
+			current := doc.Groups[len(doc.Groups)-1].Entries[address[0]]
+			for _, i := range address[1:] {
+				if current.Children == nil || len(current.Children) <= i {
+					t.Fatalf("Item was not added at specified address: %v", l)
+				}
+				current = current.Children[i]
+			}
+
+			if current.Label != l {
+				t.Fatalf("Incorrect entry found at specified address (expected \"%v\", found: \"%v\")", l, current.Label)
+			}
+		}
+
+		check(0, 0)          // x
+		check(0, 1)          // x
+		check(1, 1, 0)       //  x
+		check(1, 1, 1)       //  x
+		check(2, 1, 1, 0)    //   x
+		check(1, 1, 2)       //  x
+		check(2, 1, 2, 0)    //   x
+		check(3, 1, 2, 0, 0) //    x
+		check(3, 1, 2, 0, 1) //    x
+		check(0, 2)          // x
+		doc.AddGroup()       // ---
+		check(0, 0)          // x
+		check(0, 1)          // x
+		check(1, 1, 0)       //  x
+		check(2, 1, 0, 0)    //   x
+		check(1, 1, 1)       //  x
+		doc.AddGroup()       // ---
+		check(0, 0)          // x
+		check(1, 0, 0)       //  x
+		check(1, 0, 1)       //  x
+		check(1, 0, 2)       //  x
+		check(1, 0, 3)       //  x
+	})
+}
+
+func TestParseDocument(t *testing.T) {
 	t.Run("should require version to be declared", func(t *testing.T) {
 		produceErr(t, 2, "version",
 			"",
