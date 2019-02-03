691b975d839feb3f43f4f59d03458ac893f74148
Gabriel <gabrielj.harel@gmail.com>
2019-02-03 15:03:09 -0500

    add spinner toloading screen

--- website/components/loading/index.tsx
+++ website/components/loading/index.tsx
@@ -1,6 +1,21 @@
 import {Component} from "../../internal/types";
 import {Header} from "../header";
+import {Icon} from "../icon";
+import {styled} from "../../internal/styled";
+
+const Spinner = styled("div")({
+    opacity: 0.2,
+    textAlign: "center",
+    padding: "2rem",
+});
 
 export const Loading: Component = () => () => {
-    return <Header muted />;
+    return (
+        <div>
+            <Header muted />
+            <Spinner>
+                <Icon name="spinner" spin size={1.5} />
+            </Spinner>
+        </div>
+    );
 };
