1c62b9f3e16cf95682e112182cd6ac540d6e7e15
g-harel <gabrielj.harel@gmail.com>
2019-08-24 19:27:49 -0500

    automate certificate validation using dns record

--- terraform/aws_route53.tf
+++ terraform/aws_route53.tf
@@ -1,13 +1,3 @@
-resource "aws_acm_certificate" "ssl_cert" {
-  domain_name               = "${local.domain_name}"
-  validation_method         = "EMAIL"
-  subject_alternative_names = ["*.${local.domain_name}"]
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
 resource "aws_route53_zone" "primary" {
   name = "${local.domain_name}"
 }
@@ -34,4 +24,27 @@ resource "aws_route53_record" "cloudfront_alias" {
     name                   = "${aws_api_gateway_domain_name.domain_name.cloudfront_domain_name}"
     evaluate_target_health = false
   }
+}
+
+resource "aws_acm_certificate" "ssl_cert" {
+  domain_name               = "${local.domain_name}"
+  validation_method         = "DNS"
+  subject_alternative_names = ["*.${local.domain_name}"]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  name    = "${aws_acm_certificate.ssl_cert.domain_validation_options.0.resource_record_name}"
+  type    = "${aws_acm_certificate.ssl_cert.domain_validation_options.0.resource_record_type}"
+  zone_id = "${aws_route53_zone.primary.id}"
+  records = ["${aws_acm_certificate.ssl_cert.domain_validation_options.0.resource_record_value}"]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = "${aws_acm_certificate.ssl_cert.arn}"
+  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
 }
