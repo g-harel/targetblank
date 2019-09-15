547ca44b9c1bc5dfa0fbbb0af787da457cce673f
g-harel <gabrielj.harel@gmail.com>
2019-09-14 22:26:43 -0500

    make extension store cross-browser compatible

--- website/internal/extension.tsx
+++ website/internal/extension.tsx
@@ -1,9 +1,11 @@
 const storageKey = "targetblank-storage";
 
-export const isExtension =
-    (window as any).browser &&
-    (window as any).browser.runtime &&
-    (window as any).browser.runtime.id;
+export const isExtension = !!(
+    (window as any).chrome &&
+    (window as any).chrome.runtime &&
+    (window as any).chrome.runtime.id
+);
+export const isChromeExtension = !window.browser && isExtension;
 
 export interface ExtensionStore {
     addr: string | null;
@@ -12,10 +14,31 @@ export interface ExtensionStore {
 // Generates a zeroed-out page storage value.
 const empty = (): ExtensionStore => ({addr: null});
 
+// Wrapper around browser's `storage.sync.get` to support both Chrome and Firefox.
+const crossRead = (keys: any): Promise<any> => {
+    if (isChromeExtension) {
+        return new Promise((resolve) => {
+            (window as any).chrome.storage.sync.get(keys, resolve);
+        });
+    }
+    return browser.storage.sync.get(keys);
+};
+
+// Wrapper around browser's `storage.sync.set` to support both Chrome and Firefox.
+const crossWrite = (keys: browser.storage.StorageObject): Promise<void> => {
+    if (isChromeExtension) {
+        return new Promise((resolve) => {
+            (window as any).chrome.storage.sync.set(keys);
+            resolve();
+        });
+    }
+    return browser.storage.sync.set(keys);
+};
+
 // Reads extension data from synced browser storage.
 export const read = async (): Promise<ExtensionStore> => {
     if (!isExtension) return empty();
-    const res = await browser.storage.sync.get(storageKey);
+    const res = await crossRead(storageKey);
     return (res as any)[storageKey] || empty();
 };
 
@@ -23,5 +46,5 @@ export const read = async (): Promise<ExtensionStore> => {
 export const write = async (values: Partial<ExtensionStore>) => {
     const current = await read();
     const updated = Object.assign(current, values);
-    await browser.storage.sync.set({[storageKey]: updated});
+    await crossWrite({[storageKey]: updated});
 };
