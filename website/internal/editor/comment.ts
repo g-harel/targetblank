0eb4bbe83a45a162ac0e28063c204ee39c558e51
g-harel <gabrielj.harel@gmail.com>
2019-09-25 19:45:33 -0500

    break up comment command into internal commands

--- website/internal/editor/comment.ts
+++ website/internal/editor/comment.ts
@@ -28,77 +28,92 @@ export const toggleComment: Command = (state) => {
         comment = comment || !s.lines[i].trim().startsWith(COMMENT);
     }
 
-    if (comment) {
-        const startIndexInLine =
-            s.selectionStart - posByLine(s, selectionStartLine);
-        const endIndexInLine =
-            s.selectionEnd - posByLine(s, selectionEndLine);
-        // Find highest possible level that can be commented.
-        let level = Infinity;
-        for (let i = selectionStartLine; i <= selectionEndLine; i++) {
-            if (lineIsEmpty(s, i) && isMultilineSelection(s)) {
-                continue;
-            }
-            level = Math.min(level, leadingSpace(s, i));
+    return comment ? commentOn(state) : commentOff(state);
+};
+
+const commentOn: Command = (state) => {
+    const s = init(state);
+
+    const selectionStartLine = lineByPos(s, s.selectionStart);
+    const selectionEndLine = lineByPos(s, s.selectionEnd);
+    const startIndexInLine =
+        s.selectionStart - posByLine(s, selectionStartLine);
+    const endIndexInLine = s.selectionEnd - posByLine(s, selectionEndLine);
+
+    // Find highest possible level that can be commented.
+    let level = Infinity;
+    for (let i = selectionStartLine; i <= selectionEndLine; i++) {
+        if (lineIsEmpty(s, i) && isMultilineSelection(s)) {
+            continue;
         }
-        // Only comment at valid indentation levels.
-        level = level - (level % INDENT_LENGTH);
-        let totalAdded = 0;
-        for (let i = selectionStartLine; i <= selectionEndLine; i++) {
-            if (lineIsEmpty(s, i) && isMultilineSelection(s)) {
-                continue;
-            }
-            const line = s.lines[i];
-            s.lines[i] = `${line.slice(0, level)}${COMMENT} ${line.slice(
-                level,
-            )}`;
-            totalAdded += COMMENT_LENGTH + 1;
+        level = Math.min(level, leadingSpace(s, i));
+    }
+
+    // Only comment at valid indentation levels.
+    level = level - (level % INDENT_LENGTH);
+    let totalAdded = 0;
+    for (let i = selectionStartLine; i <= selectionEndLine; i++) {
+        if (lineIsEmpty(s, i) && isMultilineSelection(s)) {
+            continue;
         }
-        // Modify selection indecies.
-        if (startIndexInLine >= level) {
-            s.selectionStart += COMMENT_LENGTH + 1;
-        }
-        if (endIndexInLine >= level) {
-            s.selectionEnd += totalAdded;
-        } else {
-            s.selectionEnd += totalAdded - COMMENT_LENGTH - 1;
-        }
+        const line = s.lines[i];
+        s.lines[i] = `${line.slice(0, level)}${COMMENT} ${line.slice(level)}`;
+        totalAdded += COMMENT_LENGTH + 1;
+    }
+
+    // Modify selection indecies.
+    if (startIndexInLine >= level) {
+        s.selectionStart += COMMENT_LENGTH + 1;
+    }
+    if (endIndexInLine >= level) {
+        s.selectionEnd += totalAdded;
     } else {
-        const startIndexInLine =
-            s.selectionStart - posByLine(s, selectionStartLine);
-        const endIndexInLine =
-            s.selectionEnd - posByLine(s, selectionEndLine);
-        let firstLineRemoved = 0;
-        let firstLineRemovedIndex = 0;
-        let lastLineRemoved = 0;
-        let lastLineRemovedIndex = 0;
-        let totalRemoved = 0;
-        for (let i = selectionStartLine; i <= selectionEndLine; i++) {
-            if (lineIsEmpty(s, i) && isMultilineSelection(s)) {
-                continue;
-            }
-            if (i === selectionStartLine) {
-                firstLineRemovedIndex = leadingSpace(s, i);
-            }
-            if (i === selectionEndLine - 1) {
-                lastLineRemovedIndex = leadingSpace(s, i);
-            }
-            const line = s.lines[i];
-            s.lines[i] = line.replace(COMMENT_REPLACE_PATTERN, "$1");
-            const removed = line.length - s.lines[i].length;
-            totalRemoved += removed;
-            if (i === selectionStartLine) firstLineRemoved = removed;
-            if (i === selectionEndLine - 1) lastLineRemoved = removed;
+        s.selectionEnd += totalAdded - COMMENT_LENGTH - 1;
+    }
+
+    return render(s);
+};
+
+const commentOff: Command = (state) => {
+    const s = init(state);
+
+    const selectionStartLine = lineByPos(s, s.selectionStart);
+    const selectionEndLine = lineByPos(s, s.selectionEnd);
+    const startIndexInLine =
+        s.selectionStart - posByLine(s, selectionStartLine);
+    const endIndexInLine = s.selectionEnd - posByLine(s, selectionEndLine);
+
+    let firstLineRemoved = 0;
+    let firstLineRemovedIndex = 0;
+    let lastLineRemoved = 0;
+    let lastLineRemovedIndex = 0;
+    let totalRemoved = 0;
+    for (let i = selectionStartLine; i <= selectionEndLine; i++) {
+        if (lineIsEmpty(s, i) && isMultilineSelection(s)) {
+            continue;
         }
-        // Modify selection indecies.
-        if (startIndexInLine >= firstLineRemovedIndex) {
-            s.selectionStart -= firstLineRemoved;
+        if (i === selectionStartLine) {
+            firstLineRemovedIndex = leadingSpace(s, i);
         }
-        if (endIndexInLine >= lastLineRemovedIndex) {
-            s.selectionEnd -= totalRemoved;
-        } else {
-            s.selectionEnd -= totalRemoved - lastLineRemoved;
+        if (i === selectionEndLine - 1) {
+            lastLineRemovedIndex = leadingSpace(s, i);
         }
+        const line = s.lines[i];
+        s.lines[i] = line.replace(COMMENT_REPLACE_PATTERN, "$1");
+        const removed = line.length - s.lines[i].length;
+        totalRemoved += removed;
+        if (i === selectionStartLine) firstLineRemoved = removed;
+        if (i === selectionEndLine - 1) lastLineRemoved = removed;
+    }
+
+    // Modify selection indecies.
+    if (startIndexInLine >= firstLineRemovedIndex) {
+        s.selectionStart -= firstLineRemoved;
+    }
+    if (endIndexInLine >= lastLineRemovedIndex) {
+        s.selectionEnd -= totalRemoved;
+    } else {
+        s.selectionEnd -= totalRemoved - lastLineRemoved;
     }
 
     return render(s);
