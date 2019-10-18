320dea4e892ff283e6f3723fdd854689a21259c7
g-harel <gabrielj.harel@gmail.com>
2019-10-17 21:54:25 -0500

    finish migration to testing editor command testing helpers

--- website/internal/editor/indentation.test.ts
+++ website/internal/editor/indentation.test.ts
@@ -1,181 +1,113 @@
 import {indent, unindent, newline} from "./indentation";
-import {genRandomState, expectEqual} from "./testing";
+import {genRandomState, expectCommand, expectEqual} from "./testing";
 
 describe("website/internal/editor/indentation", () => {
     describe("indent", () => {
         it("should correctly add indentation on a single line", () => {
-            const value = "abc";
-            const selectionStart = 0;
-            const selectionEnd = selectionStart;
-            const expected = "    abc";
-
-            const temp = indent({value, selectionStart, selectionEnd});
-
-            expect(temp.value).toBe(expected);
+            expectCommand(indent)
+                .withValue("abc")
+                .withCursor(0)
+                .toProduceValue("    abc");
         });
 
         it("should add indentation on a single empty line", () => {
-            const value = "";
-            const selectionStart = 0;
-            const selectionEnd = selectionStart;
-            const expected = "    ";
-
-            const temp = indent({value, selectionStart, selectionEnd});
-
-            expect(temp.value).toBe(expected);
+            expectCommand(indent)
+                .withValue("")
+                .withCursor(0)
+                .toProduceValue("    ");
         });
 
         it("should snap to the nearest higher indentation level", () => {
-            const value = "  abc";
-            const selectionStart = 0;
-            const selectionEnd = selectionStart;
-            const expected = "    abc";
-
-            const temp = indent({value, selectionStart, selectionEnd});
-
-            expect(temp.value).toBe(expected);
+            expectCommand(indent)
+                .withValue("  abc")
+                .withCursor(0)
+                .toProduceValue("    abc");
         });
 
         it("should indent all lines when multiple selected", () => {
-            const value = "abc\n    123\n456\n    xyz";
-            const selectionStart = 6;
-            const selectionEnd = 14;
-            const expected = "abc\n        123\n    456\n    xyz";
-
-            const temp = indent({value, selectionStart, selectionEnd});
-
-            expect(temp.value).toBe(expected);
+            expectCommand(indent)
+                .withValue("abc\n    123\n456\n    xyz")
+                .withSelection(6, 14)
+                .toProduceValue("abc\n        123\n    456\n    xyz");
         });
 
         it("should not indent empty lines when multiple selected", () => {
-            const value = "abc\n\n123";
-            const selectionStart = 2;
-            const selectionEnd = 6;
-            const expected = "    abc\n\n    123";
-
-            const temp = indent({value, selectionStart, selectionEnd});
-
-            expect(temp.value).toBe(expected);
+            expectCommand(indent)
+                .withValue("abc\n\n123")
+                .withSelection(2, 6)
+                .toProduceValue("    abc\n\n    123");
         });
 
         it("should adjust cursor position", () => {
-            const value = "  123";
-            const selectionStart = 2;
-            const selectionEnd = selectionStart;
-            const expectedCursor = 4;
-
-            const temp = indent({value, selectionStart, selectionEnd});
-
-            expect(temp.selectionStart).toBe(expectedCursor);
-            expect(temp.selectionEnd).toBe(expectedCursor);
+            expectCommand(indent)
+                .withValue("  123")
+                .withCursor(2)
+                .toProduceCursor(4);
         });
 
         it("should adjust cursor position when multiple selected", () => {
-            const value = "  abc\n  123\n 456\nxyz";
-            const selectionStart = 10;
-            const selectionEnd = 18;
-            const expectedStart = 12;
-            const expectedEnd = 27;
-
-            const temp = indent({value, selectionStart, selectionEnd});
-
-            expect(temp.selectionStart).toBe(expectedStart);
-            expect(temp.selectionEnd).toBe(expectedEnd);
+            expectCommand(indent)
+                .withValue("  abc\n  123\n 456\nxyz")
+                .withSelection(10, 18)
+                .toProduceSelection(12, 27);
         });
     });
 
     describe("unindent", () => {
         it("should correctly remove indentation from single line", () => {
-            const value = "    abc";
-            const selectionStart = 0;
-            const selectionEnd = selectionStart;
-            const expected = "abc";
-
-            const temp = unindent({value, selectionStart, selectionEnd});
-
-            expect(temp.value).toBe(expected);
+            expectCommand(unindent)
+                .withValue("    abc")
+                .withCursor(0)
+                .toProduceValue("abc");
         });
 
         it("should make not changes if line is not indented", () => {
-            const value = "abc";
-            const selectionStart = 0;
-            const selectionEnd = selectionStart;
-            const expected = value;
-
-            const temp = unindent({value, selectionStart, selectionEnd});
-
-            expect(temp.value).toBe(expected);
+            expectCommand(unindent)
+                .withValue("abc")
+                .withCursor(0)
+                .toProduceUnchanged();
         });
 
         it("should snap to the nearest lower indentation level", () => {
-            const value = "      abc";
-            const selectionStart = 0;
-            const selectionEnd = selectionStart;
-            const expected = "    abc";
-
-            const temp = unindent({value, selectionStart, selectionEnd});
-
-            expect(temp.value).toBe(expected);
+            expectCommand(unindent)
+                .withValue("       abc")
+                .withCursor(0)
+                .toProduceValue("    abc");
         });
 
         it("should un-indent all selected lines", () => {
-            const value = "abc\n    123\n456\n    xyz";
-            const selectionStart = 6;
-            const selectionEnd = 14;
-            const expected = "abc\n123\n456\n    xyz";
-
-            const temp = unindent({value, selectionStart, selectionEnd});
-
-            expect(temp.value).toBe(expected);
+            expectCommand(unindent)
+                .withValue("abc\n    123\n456\n    xyz")
+                .withSelection(6, 14)
+                .toProduceValue("abc\n123\n456\n    xyz");
         });
 
         it("should ignore empty lines when un-indenting", () => {
-            const value = "    abc\n\n        123";
-            const selectionStart = 3;
-            const selectionEnd = 19;
-            const expected = "abc\n\n    123";
-
-            const temp = unindent({value, selectionStart, selectionEnd});
-
-            expect(temp.value).toBe(expected);
+            expectCommand(unindent)
+                .withValue("    abc\n\n        123")
+                .withSelection(3, 19)
+                .toProduceValue("abc\n\n    123");
         });
 
         it("should adjust cursor position", () => {
-            const value = "        123";
-            const selectionStart = 7;
-            const selectionEnd = selectionStart;
-            const expectedCursor = 3;
-
-            const temp = unindent({value, selectionStart, selectionEnd});
-
-            expect(temp.selectionStart).toBe(expectedCursor);
-            expect(temp.selectionEnd).toBe(expectedCursor);
+            expectCommand(unindent)
+                .withValue("        123")
+                .withCursor(7)
+                .toProduceCursor(3);
         });
 
         it("should adjust cursor position when multiple selected", () => {
-            const value = "  abc\n    123\n  456\n      xyz";
-            const selectionStart = 10;
-            const selectionEnd = 25;
-            const expectedStart = 6;
-            const expectedEnd = 17;
-
-            const temp = unindent({value, selectionStart, selectionEnd});
-
-            expect(temp.selectionStart).toBe(expectedStart);
-            expect(temp.selectionEnd).toBe(expectedEnd);
+            expectCommand(unindent)
+                .withValue("  abc\n    123\n  456\n      xyz")
+                .withSelection(10, 25)
+                .toProduceSelection(6, 17);
         });
 
         it("should not change the cursor's line when it bottoms out", () => {
-            const value = "  abc\n    123\n  456\n      xyz";
-            const selectionStart = 6;
-            const selectionEnd = 21;
-            const expectedStart = 6;
-            const expectedEnd = 14;
-
-            const temp = unindent({value, selectionStart, selectionEnd});
-
-            expect(temp.selectionStart).toBe(expectedStart);
-            expect(temp.selectionEnd).toBe(expectedEnd);
+            expectCommand(unindent)
+                .withValue("  abc\n    123\n  456\n      xyz")
+                .withSelection(6, 21)
+                .toProduceSelection(6, 14);
         });
     });
 
@@ -193,83 +125,52 @@ describe("website/internal/editor/indentation", () => {
 
     describe("newline", () => {
         it("should add a new line", () => {
-            const value = "abc";
-            const selectionStart = 3;
-            const selectionEnd = selectionStart;
-            const expected = "abc\n";
-
-            const temp = newline({value, selectionStart, selectionEnd});
-
-            expect(temp.value).toBe(expected);
+            expectCommand(newline)
+                .withValue("abc")
+                .withCursor(3)
+                .toProduceValue("abc\n");
         });
 
         it("should keep the same indentation", () => {
-            const value = "   abc";
-            const selectionStart = 6;
-            const selectionEnd = selectionStart;
-            const expected = "   abc\n   ";
-
-            const temp = newline({value, selectionStart, selectionEnd});
-
-            expect(temp.value).toBe(expected);
+            expectCommand(newline)
+                .withValue("   abc")
+                .withCursor(6)
+                .toProduceValue("   abc\n   ");
         });
 
         it("should keep the same indentation when cursor in whitespace", () => {
-            const value = "     123";
-            const selectionStart = 3;
-            const selectionEnd = selectionStart;
-            const expected = "   \n     123";
-
-            const temp = newline({value, selectionStart, selectionEnd});
-
-            expect(temp.value).toBe(expected);
+            expectCommand(newline)
+                .withValue("     123")
+                .withCursor(3)
+                .toProduceValue("   \n     123");
         });
 
         it("should split up lines when cursor in content", () => {
-            const value = "  123";
-            const selectionStart = 4;
-            const selectionEnd = selectionStart;
-            const expected = "  12\n  3";
-
-            const temp = newline({value, selectionStart, selectionEnd});
-
-            expect(temp.value).toBe(expected);
+            expectCommand(newline)
+                .withValue("  123")
+                .withCursor(4)
+                .toProduceValue("  12\n  3");
         });
 
         it("should add new line after the end of the selection", () => {
-            const value = "abc\n    123\n 456\nxyz";
-            const selectionStart = 10;
-            const selectionEnd = 14;
-            const expected = "abc\n    123\n 4\n 56\nxyz";
-
-            const temp = newline({value, selectionStart, selectionEnd});
-
-            expect(temp.value).toBe(expected);
+            expectCommand(newline)
+                .withValue("abc\n    123\n 456\nxyz")
+                .withSelection(10, 14)
+                .toProduceValue("abc\n    123\n 4\n 56\nxyz");
         });
 
         it("should adjust cursor position", () => {
-            const value = "    123";
-            const selectionStart = 4;
-            const selectionEnd = selectionStart;
-            const expectedCursor = 9;
-
-            const temp = newline({value, selectionStart, selectionEnd});
-
-            expect(temp.selectionStart).toBe(expectedCursor);
-            expect(temp.selectionEnd).toBe(expectedCursor);
+            expectCommand(newline)
+                .withValue("    123")
+                .withCursor(4)
+                .toProduceCursor(9);
         });
 
         it("should adjust cursor position when multiple selected", () => {
-            const value = "  abc\n    123\n  456\n      xyz";
-            const selectionStart = 10;
-            const selectionEnd = 25;
-            const expectedStart = 31;
-            const expectedEnd = expectedStart;
-
-            const temp = newline({value, selectionStart, selectionEnd});
-
-            expect(temp.selectionStart).toBe(expectedStart);
-            expect(temp.selectionEnd).toBe(expectedEnd);
+            expectCommand(newline)
+                .withValue("  abc\n    123\n  456\n      xyz")
+                .withSelection(10, 25)
+                .toProduceSelection(31, 31);
         });
     });
 });
