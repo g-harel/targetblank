dee71e8881db668d1986bc20b87c4569d955e5ef
g-harel <gabrielj.harel@gmail.com>
2020-06-23 22:56:54 -0500

    add styling to alert chips

--- website/components/page/chips.tsx
+++ website/components/page/chips.tsx
@@ -4,26 +4,35 @@ import {Component} from "../../internal/types";
 
 const chips: Array<{
     text: string;
-}> = [];
+}> = [
+    // {text: "You're already logged in!"},
+    // {text: "This comment is multi-line because I need to check out what that would look like."},
+    // {text: "This one shows how the chips look like when there are many of them."},
+];
 
 const Wrapper = styled("div")({
-    color: color.textSecondarySmall,
-    height: 0,
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "column",
     fontSize: size.normal,
+    height: 0,
+    zIndex: 1,
 });
 
 const Chip = styled("div")({
-    border: "1px solid red",
-    margin: "1rem",
-    padding: "1rem",
+    backgroundColor: color.backgroundPrimary,
+    border: "1px solid transparent",
+    borderRadius: "2px",
+    boxShadow: `0 0 0.5px 1px ${color.decoration}`,
+    color: color.textSecondarySmall,
+    fontWeight: 600,
+    marginTop: "1rem",
+    padding: "0.5rem 0.5rem 0.6rem",
+    maxWidth: "30rem",
 });
 
 export interface Props {}
 
 export const Chips: Component<Props> = () => () => (
-    <Wrapper>
-        {...chips.map((chip) => (
-            <Chip>{chip.text}</Chip>
-        ))}
-    </Wrapper>
+    <Wrapper>{...chips.map((chip) => <Chip>{chip.text}</Chip>)}</Wrapper>
 );
