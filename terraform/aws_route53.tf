7832de1e084cdfb295de0d8b13535e5d68f11d07
Gabriel Harel <gabrielj.harel@gmail.com>
2022-04-26 22:57:51 -0500

    revert cert validation to use only first option

--- terraform/aws_route53.tf
+++ terraform/aws_route53.tf
@@ -37,18 +37,10 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  name    = each.value.name
-  type    = each.value.type
+  name    = "${element(aws_acm_certificate.ssl_cert.domain_validation_options, 0).resource_record_name}"
+  type    = "${element(aws_acm_certificate.ssl_cert.domain_validation_options, 0).resource_record_type}"
   zone_id = "${aws_route53_zone.primary.id}"
-  records = [each.value.record]
+  records = ["${element(aws_acm_certificate.ssl_cert.domain_validation_options, 0).resource_record_value}"]
   ttl     = 60
 }
 
